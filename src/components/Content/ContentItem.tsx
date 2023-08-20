d69e4d41c38178a04e761e2cb8c2e775580c7cf7
Alexey <57610468+AlexeyVolnov@users.noreply.github.com>
2023-08-20 23:38:00 +0500

    1
    1

--- src/components/Content/ContentItem.tsx
+++ src/components/Content/ContentItem.tsx
@@ -1,24 +1,30 @@
-import {FC} from "react";
+import {createRef, FC, useEffect, useRef} from "react";
 import DateLine from "../Working/DateLine.tsx";
 import {IWorkAndEducation} from "../Working/Work.tsx";
 
 
 interface IContentItemProps {
-item:IWorkAndEducation
+  item: IWorkAndEducation
 }
 
 
 const ContentItem: FC<IContentItemProps> = ({item}) => {
+
+  const refBlockText = createRef();
+const resizeSvg = ()=>{
+
+}
+
   return (
      <div>
        <div className={'flex gap-5'}>
          <div className={''}>
-           <DateLine startDate={item.startDate} finalDate={item.finalDate}/>
+           <DateLine startDate={item.startDate} refBlockText={refBlockText} resizeSvg={}  finalDate={item.finalDate}/>
          </div>
-         <div className={'work_info justify-center flex flex-col flex-1'}>
+         <div ref={refBlockText} className={'work_info justify-center flex flex-col flex-1'}>
            <h4 className={'font-bold text-2xl text-[#2b384a] py-3.5'}>{item.Title}</h4>
-             <p>{item.nameOrganization}</p>
-             <p className={'py-5 text-gray-700 font-thin'}>{item.Text}</p>
+           <p>{item.nameOrganization}</p>
+           <p className={'py-5 text-gray-700 font-thin'}>{item.Text}</p>
          </div>
        </div>
      </div>
--- src/components/Working/DateLine.tsx
+++ src/components/Working/DateLine.tsx
@@ -1,31 +1,52 @@
 import ContentLoader from "react-content-loader"
-import {FC} from "react";
+import {FC, useEffect, useState} from "react";
 
 interface IDateLine {
   startDate: number,
-  finalDate: number
+  finalDate: number,
+  refBlockText:any,
+  resizeSvg:()=>any
 }
 
 
-const DateLine: FC<IDateLine> = ({startDate, finalDate}) => (
-   <ContentLoader
-      speed={7}
-      width={150}
-      height={310}
-      viewBox="0 0 150 310"
-      backgroundColor="#666d8f"
-      foregroundColor="#ecd4d4"
-   >
+const DateLine: FC<IDateLine> = ({startDate, finalDate ,refBlockText,resizeSvg}) =>{
+const [svgH,setSvgH] = useState(253)
+  console.log(refBlockText.current)
 
-     <rect x="20" y="0" rx="0" ry="0" width="3" height="283" />
-     <circle cx="22" cy="40" r="6"/>
-     <circle cx="22" cy="260" r="6"/>
-     <rect width='100' opacity={0.1} x='40' y="5" cx="80" cy="50" ry="5" height='60'></rect>
-     <text x={70} y={257} color="black" fontWeight="bold">{startDate}</text>
-     <rect width='100' opacity={0.1} x='40' y="220" cx="80" cy="50" ry="5" height='60'></rect>
-     <text x={70} y={40} color="black" fontWeight="bold">{finalDate}</text>
+  useEffect(()=>{
+    resizeSvg()
+  },[refBlockText])
 
-   </ContentLoader>
-)
+  return(
+     /*<ContentLoader
+        speed={7}
+        width={150}
+        height={310}
+        viewBox="0 0 150 310"
+        backgroundColor="#666d8f"
+        foregroundColor="#ecd4d4"
+     >
+
+       <rect x="20" y="0" rx="0" ry="0" width="3" height="283" />
+       <circle cx="22" cy="40" r="6"/>
+       <circle cx="22" cy="260" r="6"/>
+       <rect width='100' opacity={0.1} x='40' y="5" cx="80" cy="50" ry="5" height='60'></rect>
+       <text x={70} y={257} color="black" fontWeight="bold">{startDate}</text>
+       <rect width='100' opacity={0.1} x='40' y="220" cx="80" cy="50" ry="5" height='60'></rect>
+       <text x={70} y={40} color="black" fontWeight="bold">{finalDate}</text>
+
+     </ContentLoader>*/
+     <div className={'relative'}>
+       <svg className={'absolute'} width="48" height="254" viewBox="0 0 48 254" fill="none" xmlns="http://www.w3.org/2000/svg">
+         <path d={`M48 1H0.5V${svgH}.5H48`} stroke="#C4C0C0"/>
+       </svg>
+     </div>
+  )
+}
+
+
+
+
+
 
 export default DateLine
